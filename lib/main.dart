793c89a1d03e9bacf6c3827fa310303c8e88fe5a
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-05-14 02:36:11 +0900

    add bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: '習慣マネージャー'),
     );
   }
 }
@@ -45,6 +45,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _selectedTabIndex = 0;
+
+  List _pages = [
+    Text("ホーム"),
+    Text("記録"),
+    Text("登録"),
+  ];
+
+  _changeIndex(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -66,41 +79,20 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(child: _pages[_selectedTabIndex]), 
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTabIndex, 
+        onTap: _changeIndex, 
+        type: BottomNavigationBarType.fixed, 
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("ホーム")), 
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text("記録")), 
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), title: Text("登録")), 
+        ], 
+      ), 
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
