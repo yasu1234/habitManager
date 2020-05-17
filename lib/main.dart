bf1645c8a0c269c0aee4dd4191af0fe2bccf7ce1
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-05-17 23:19:03 +0900

    delete floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -93,11 +93,6 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: Icon(Icons.shopping_cart), title: Text("登録")), 
         ], 
       ), 
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
