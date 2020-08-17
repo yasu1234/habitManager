9237bdc481e9d5b9a515737b2cd19245fc9d5f4b
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-08-18 00:34:56 +0900

    add routing function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habitmanager/main_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,88 +21,11 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: '習慣マネージャー'),
-      // routes:<String, WidgetBuilder> {
-      //   '/record' : (context) => RecordPage(),
-      //   '/setting' : (context) => SettingPage()
-      // }
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int _selectedTabIndex = 0;
-
-  List _pages = [
-    Text("ホームで表示される"),
-    Text("記録で表示される"),
-    Text("登録で表示される")
-  ];
-
-  _changeIndex(int index) {
-    setState(() {
-      _selectedTabIndex = index;
-
-      // if (_selectedTabIndex == 1) {
-      //   Navigator.of(context).pushNamed("/record");
-      // } else {
-      //   Navigator.of(context).pushNamed("/setting");
-      // }
-
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(child: _pages[_selectedTabIndex]), 
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedTabIndex, 
-        onTap: _changeIndex, 
-        type: BottomNavigationBarType.fixed, 
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("ホーム")), 
-          BottomNavigationBarItem(icon: Icon(Icons.edit), title: Text("記録")),
-          BottomNavigationBarItem(icon: Icon(Icons.data_usage), title: Text("登録")),
-          ], 
-      ), 
+      // home: MyHomePage(title: '習慣マネージャー'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MainPage(),
+      },
     );
   }
 }
