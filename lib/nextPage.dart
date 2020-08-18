cc53863df9109121534071bd08d2b13fdf8225aa
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-08-18 13:17:45 +0900

    add nextPage that display to tap button in home view

---
+++ lib/nextPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("次のページの画面だよ！！"),
+    );
+  }
+}
